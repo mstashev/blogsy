f52e10098d845198c6245c189c46bf1858161eee
Michael Stashevsky <mstashev@gmail.com>
2017-02-28 13:39:20 -0500

    Fixed tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,5 +6,4 @@ class PostTest < ActiveSupport::TestCase
   should validate_presence_of :title
   should validate_presence_of :body
   
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,15 +6,4 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of :email_address
   should validate_presence_of :username
 
-  # def setup
-  #   @user = FactoryGirl.build(:user)
-  # end
-  #
-  # test "Should have an email" do
-  #   @user.email_address = nil
-  #   refute @user.save
-  # end
-
-
-
 end
